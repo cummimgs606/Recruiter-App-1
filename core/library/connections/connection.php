8ad4cbda09e5ff634fbb70f485b895519757705c
cummings606 <cummings606@hotmail.com>
2018-10-03 16:36:11 +0100

    Removed Secure Data

--- core/library/connections/connection.php
+++ core/library/connections/connection.php
@@ -5,21 +5,12 @@
 class Connection {
 	
 	
-	protected static $name 		= 'HNCOM';
-	protected static $hostname 	= '10.3.5.57';
+	protected static $name 		= '';
+	protected static $hostname 	= '';
 	protected static $port 		= '';
-	protected static $database 	= 'hncom';
-	protected static $username	= 'HNCOM';
-	protected static $password 	= 'NyIRV2POeOcQYx1jDzwI';
-	
-/*
-	protected static $name 		= 'HNCOM';
-	protected static $hostname 	= 'hncom.ddns.net';
-	protected static $port 		= '3306';
-	protected static $database 	= 'hncom';
-	protected static $username	= 'HNCOM';
-	protected static $password 	= 'vHF7x3n9f5saU4n';
-*/
+	protected static $database 	= '';
+	protected static $username	= '';
+	protected static $password 	= '';
 }
 ?>
 
